33dd92bd6f992f44ecd129269de4353933ea2805
Pano <candywanwan1996@gmail.com>
2026-05-11 22:18:01 +0900

    DirectX12フレームワークの初期プロジェクト構成を作成

---
+++ Src/Graphics/graphics_device.cpp
@@ -0,0 +1,104 @@
+﻿#include "graphics_device.h"
+
+bool GraphicsDevice::Initialize(bool enable_debug_layer)
+{
+    if (enable_debug_layer)
+    {
+        ComPtr<ID3D12Debug> debug_controller;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_controller))))
+        {
+            debug_controller->EnableDebugLayer();
+        }
+    }
+    if (!CreateFactory(enable_debug_layer))
+    {
+        MessageBox(NULL, L"Failed to create DXGI factory", L"Error", MB_OK);
+        return false;
+    }
+    if (!CreateAdapter())
+    {
+        MessageBox(NULL, L"Failed to create DXGI adapter", L"Error", MB_OK);
+        return false;
+    }
+    if (!CreateDevice())
+    {
+        MessageBox(NULL, L"Failed to create D3D12 device", L"Error", MB_OK);
+        return false;
+    }
+
+    return true;
+}
+
+ID3D12Device* GraphicsDevice::GetDevice() const
+{
+    return device_.Get();   
+}
+
+IDXGIFactory6* GraphicsDevice::GetFactory() const
+{
+    return factory_.Get();  
+}
+
+bool GraphicsDevice::CreateFactory(bool enable_debug_layer)
+{
+    UINT factory_flag = 0;
+    if (enable_debug_layer)
+    {
+        factory_flag |= DXGI_CREATE_FACTORY_DEBUG;
+    }
+    HRESULT hr = CreateDXGIFactory2(factory_flag, IID_PPV_ARGS(&factory_));
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool GraphicsDevice::CreateAdapter()
+{
+    for (UINT index = 0; ; ++index)
+    {
+        ComPtr<IDXGIAdapter1> adapter;
+        HRESULT hr = factory_->EnumAdapterByGpuPreference(
+            index,
+            DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
+            IID_PPV_ARGS(&adapter));
+        if (hr == DXGI_ERROR_NOT_FOUND)
+        {
+            break;
+        }
+        if (FAILED(hr))
+        {
+            return false;
+        }
+
+        DXGI_ADAPTER_DESC1 desc;
+        adapter->GetDesc1(&desc);
+
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+        {
+            continue;
+        }
+
+        hr = D3D12CreateDevice(
+            adapter.Get(), D3D_FEATURE_LEVEL_12_0, _uuidof(ID3D12Device),
+            nullptr);
+        if (SUCCEEDED(hr))
+        {
+            adapter_ = adapter;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool GraphicsDevice::CreateDevice()
+{
+    HRESULT hr = device_->QueryInterface(IID_PPV_ARGS(&device_));
+    if (FAILED(hr))
+    {
+        return false;
+    }
+    return true;   
+}
---
+++ Src/Platform/window.cpp
@@ -0,0 +1,142 @@
+#include "Window.h"
+
+Window::Window()
+{
+}
+
+Window::~Window()
+{
+    if (hwnd_ != nullptr)
+    {
+        DestroyWindow(hwnd_);
+        hwnd_ = nullptr;
+    }
+
+    UnregisterClass(class_name_.c_str(), instance_);
+}
+
+bool Window::Create(const wchar_t* title, int width, int height)
+{
+    size_.height = height;
+    size_.width = width;
+    // CreateWindowExWで使うウィンドウクラスを先に登録する。
+    WNDCLASSEXW window_class = {};
+    window_class.cbSize = sizeof(WNDCLASSEX);
+    window_class.style = CS_HREDRAW | CS_VREDRAW;
+    window_class.lpfnWndProc = WndProc;
+    window_class.hInstance = instance_;
+    window_class.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    window_class.lpszClassName = class_name_.c_str();
+    if (!RegisterClassEx(&window_class))
+    {
+        return false;
+    }
+
+    // 呼び出し側が指定した描画領域サイズを、Win32のRECTに変換する。
+    RECT rect = {
+        0, 0,
+        static_cast<LONG>(width), static_cast<LONG>(height)
+    };
+
+    // CreateWindowExに渡すため、外枠とタイトルバー込みのサイズへ補正する。
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int window_width = rect.right - rect.left;
+    int window_height = rect.bottom - rect.top;
+    
+    // WndProcでこのC++オブジェクトを取り出せるよう、最後の引数にthisを渡す。
+    hwnd_ = CreateWindowEx(0, class_name_.c_str(), title,
+                           WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                           window_width, window_height, nullptr, nullptr, instance_, this);
+
+    return hwnd_ != nullptr;
+}
+
+void Window::Show()
+{
+    ShowWindow(hwnd_, SW_SHOW);
+    UpdateWindow(hwnd_);
+}
+
+bool Window::ProcessMessages()
+{
+    MSG msg = {};
+    // メッセージがない間もメインループを止めないため、PeekMessageを使う。
+    while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+    {
+        // WM_QUITを受け取ったら、アプリケーションのメインループを終了する。
+        if (msg.message == WM_QUIT)
+            return false;
+
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return !closed_;
+}
+
+HWND Window::GetHwnd() const
+{
+    return hwnd_;
+}
+
+uint32_t Window::GetWidth() const
+{
+    return size_.width;
+}
+
+uint32_t Window::GetHeight() const
+{
+    return size_.height;
+}
+
+WindowSize Window::GetSize() const
+{
+    return size_;
+}
+
+LRESULT Window::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    Window* window = nullptr;
+    // 最初の生成メッセージには、CreateWindowExで渡したthisが入っている。
+    if (msg == WM_NCCREATE)
+    {
+        auto* create_struct = reinterpret_cast<CREATESTRUCTW*>(lParam);
+        window = static_cast<Window*>(create_struct->lpCreateParams);
+        // 次回以降のメッセージで使えるよう、HWNDにthisを保存する。
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window));
+        window->hwnd_ = hwnd;
+    }
+    else
+    {
+        window = reinterpret_cast<Window*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+    }
+
+    if (window != nullptr)
+    {
+        return window->HandleMessage(msg, wParam, lParam);
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMessage(UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    // 自分で処理したいWin32メッセージだけここで扱う。
+    switch (msg)
+    {
+    case WM_CLOSE:
+        closed_ = true;
+        DestroyWindow(hwnd_);
+        hwnd_ = nullptr;
+        return 0;
+    case WM_DESTROY:
+        closed_ = true;
+        PostQuitMessage(0);
+        return 0;
+    case WM_SIZE:
+        size_.width = LOWORD(lParam);
+        size_.height = HIWORD(lParam);
+        return 0;
+    }
+    // 未処理のメッセージはWin32の標準処理に任せる。
+    return DefWindowProc(hwnd_, msg, wParam, lParam);
+}
