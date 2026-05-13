402ba050fe775025ddea5d3c0abcf6c6a6806a3c
Pano <candywanwan1996@gmail.com>
2026-05-13 22:59:40 +0900

    DirectX12のCommandListクラスを追加

--- Src/Graphics/command_list.cpp
+++ Src/Graphics/command_list.cpp
@@ -1,1 +1,99 @@
-﻿#include "command_list.h"
+#include "command_list.h"
+
+CommandList::CommandList()
+{
+}
+
+CommandList::~CommandList()
+{
+}
+
+bool CommandList::Initialize(ID3D12Device* device)
+{
+    if (!CreateCommandAllocator(device))
+    {
+        MessageBox(NULL, L"Failed to create command allocator", L"Error", MB_OK);
+        return false;
+    }
+
+    if (!CreateCommandList(device))
+    {
+        MessageBox(NULL, L"Failed to create command list", L"Error", MB_OK);
+        return false;
+    }
+
+    return true;
+}
+
+bool CommandList::Reset()
+{
+    HRESULT hr = command_allocator_->Reset();
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    hr = command_list_->Reset(command_allocator_.Get(), nullptr);
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool CommandList::Close()
+{
+    HRESULT hr = command_list_->Close();
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+ID3D12GraphicsCommandList* CommandList::GetCommandList() const
+{
+    return command_list_.Get();
+}
+
+ID3D12CommandAllocator* CommandList::GetCommandAllocator() const
+{
+    return command_allocator_.Get();
+}
+
+bool CommandList::CreateCommandAllocator(ID3D12Device* device)
+{
+    HRESULT hr = device->CreateCommandAllocator(
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        IID_PPV_ARGS(&command_allocator_));
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool CommandList::CreateCommandList(ID3D12Device* device)
+{
+    HRESULT hr = device->CreateCommandList(
+        0,
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        command_allocator_.Get(),
+        nullptr,
+        IID_PPV_ARGS(&command_list_));
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    hr = command_list_->Close();
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    return true;
+}
--- Src/Graphics/command_list.h
+++ Src/Graphics/command_list.h
@@ -1,7 +1,23 @@
-﻿#pragma once
+#pragma once
+#include "../Core/common.h"
 
 class CommandList
 {
 public:
-    
+    CommandList();
+    ~CommandList();
+
+    bool Initialize(ID3D12Device* device);
+    bool Reset();
+    bool Close();
+
+    ID3D12GraphicsCommandList* GetCommandList() const;
+    ID3D12CommandAllocator* GetCommandAllocator() const;
+
+private:
+    bool CreateCommandAllocator(ID3D12Device* device);
+    bool CreateCommandList(ID3D12Device* device);
+
+    ComPtr<ID3D12CommandAllocator> command_allocator_;
+    ComPtr<ID3D12GraphicsCommandList> command_list_;
 };
