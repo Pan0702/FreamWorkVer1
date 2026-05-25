11d4e4e3cdac2efb3039e74947df37b64c1972ce
Pano <candywanwan1996@gmail.com>
2026-05-26 08:49:21 +0900

    Dear ImGui の関連ヘッダーおよびソースファイルを追加し、ImGuiManager クラスを新規実装して描画フレーム管理機能を統合

---
+++ Src/Debug/ImGui/imgui_manager.cpp
@@ -0,0 +1,17 @@
+﻿#include "imgui_manager.h"
+
+bool ImGuiManager::Initialize(HWND hwnd, ID3D12Device* device, int numFramesInFlight)
+{
+}
+
+void ImGuiManager::BeginFrame()
+{
+}
+
+void ImGuiManager::EndFrame(ID3D12GraphicsCommandList* command_list)
+{
+}
+
+void ImGuiManager::Shutdown()
+{
+}
---
+++ Src/Debug/ImGui/imgui_manager.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "../../Platform/Window.h"
+
+class ImGuiManager
+{
+public:
+    bool Initialize(HWND hwnd,ID3D12Device* device,int numFramesInFlight);
+    void BeginFrame();
+    void EndFrame(ID3D12GraphicsCommandList* command_list);
+    void Shutdown();
+    
+    private:
+    ComPtr<ID3D12DescriptorHeap> srv_heap_;
+};
